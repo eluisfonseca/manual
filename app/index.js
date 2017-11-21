605aa359201c32de344c3edb38f0b8cbb1062f55
Eduardo Fonseca <eduardo.fonseca@tekzenit.com>
2017-11-21 19:53:56 +0000

    Add resources. Worked on menu and sections

--- app/index.js
+++ app/index.js
@@ -5,6 +5,25 @@
 // Load application styles
 import 'styles/index.scss';
 
+// Load js libraries
+import ScrollSnap from 'scroll-snap'
+
 // ================================
 // START YOUR APP HERE
 // ================================
+const snapConfig = {
+  scrollSnapDestination: '90% 0%', // *REQUIRED* scroll-snap-destination css property, as defined here: https://developer.mozilla.org/en-US/docs/Web/CSS/scroll-snap-destination
+  scrollTimeout: 100, // *OPTIONAL* (default = 100) time in ms after which scrolling is considered finished
+  scrollTime: 300 // *OPTIONAL* (default = 300) time in ms for the smooth snap
+}
+ 
+function callback () {
+  console.log('called when snap animation ends')
+}
+ 
+const element = document.getElementById('content')
+const snapObject = new ScrollSnap(element, snapConfig)
+ 
+snapObject.bind(callback)
+
+console.log('ola');
