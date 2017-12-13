6c949539edac8664747f23c87b2292740e614ea4
Eduardo Fonseca <eduardo.fonseca@tekzenit.com>
2017-12-13 14:06:47 +0000

    Cleans images and SCSS. Adds mobile menu. Adds mobile landing page. Adds Gent section. Adds waypoints

--- app/index.js
+++ app/index.js
@@ -5,25 +5,28 @@
 // Load application styles
 import 'styles/index.scss';
 
-// Load js libraries
-import ScrollSnap from 'scroll-snap'
-
-// ================================
-// START YOUR APP HERE
-// ================================
-const snapConfig = {
-  scrollSnapDestination: '90% 0%', // *REQUIRED* scroll-snap-destination css property, as defined here: https://developer.mozilla.org/en-US/docs/Web/CSS/scroll-snap-destination
-  scrollTimeout: 100, // *OPTIONAL* (default = 100) time in ms after which scrolling is considered finished
-  scrollTime: 300 // *OPTIONAL* (default = 300) time in ms for the smooth snap
-}
- 
-function callback () {
-  console.log('called when snap animation ends')
-}
- 
-const element = document.getElementById('content')
-const snapObject = new ScrollSnap(element, snapConfig)
- 
-snapObject.bind(callback)
-
 console.log('ola');
+
+var waypointLanding = new Waypoint({
+  element: document.getElementById('gent'),
+  handler: function(direction) {
+    console.log('You have scrolled')
+    document.getElementById('first-menu-mobile').className = document.getElementById('first-menu-mobile').className.replace(/fixed/g,'');
+    document.getElementById('gent-button').className = document.getElementById('gent-button').className.replace(/active/g,'');
+  },
+  offset: '25%'
+})
+
+var waypointGent = new Waypoint({
+  element: document.getElementById('gent'),
+  handler: function(direction) {
+    if (document.getElementById('gent-button').className.indexOf(' active') == -1) {
+      document.getElementById('gent-button').className = " active";
+    }
+    if (document.getElementById('first-menu-mobile').className.indexOf(' fixed') == -1) {
+      document.getElementById('first-menu-mobile').className += " fixed";
+    }
+    console.log('You have scrolled to a thing')
+  },
+  offset: 'bottom-in-view'
+})
